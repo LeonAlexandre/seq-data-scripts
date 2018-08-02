e2d1917f72bc6190b549535ce272af44a5f0fd47
Alex <alexandre.leon.contact@gmail.com>
2018-08-01 17:16:42 -0700

    smol changes

--- script.sh
+++ script.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
 #make it a while loop
-over=0.1
-for i in {1..5};
-do foo="newdata$i";
+
+foo='/Users/alex/Desktop/seq100by20_03_biadam_bahdanau'
+
 echo $foo; 
-python ./datagenCutter.py --out_dir=$foo --overlap=$over
 
-python ./assemble.py --outdir=$foo --f_inference="$foo/frag_train.label" --fragnum=5 --mode="both" --bias=sin
+python ./assemble.py --outdir=$foo --f_inference="$foo/output_test" --fragnum=5 --mode="both" --bias=sin
+
 echo "edit sin"
-python report.py --outdir=$foo --assembled=$foo/recons_edit_sin.txt --labels=$foo/train.label --mode=edit --bias=sin
+python report.py --outdir=$foo --assembled=$foo/recons_edit_sin.txt --labels=$foo/test.label --mode=edit --bias=sin
 echo "hamming sin"
-python report.py --outdir=$foo --assembled=$foo/recons_hamming_sin.txt --labels=$foo/train.label --mode=hamming --bias=sin
+python report.py --outdir=$foo --assembled=$foo/recons_hamming_sin.txt --labels=$foo/test.label --mode=hamming --bias=sin
 
-python ./assemble.py --outdir=$foo --f_inference="$foo/frag_train.label" --fragnum=5 --mode="both" --bias=log
+python ./assemble.py --outdir=$foo --f_inference="$foo/output_test" --fragnum=5 --mode="both" --bias=log
 echo "edit log"
-python report.py --outdir=$foo --assembled=$foo/recons_edit_log.txt --labels=$foo/train.label --mode=edit --bias=log
+python report.py --outdir=$foo --assembled=$foo/recons_edit_log.txt --labels=$foo/test.label --mode=edit --bias=log
 echo "hamming log"
-python report.py --outdir=$foo --assembled=$foo/recons_hamming_log.txt --labels=$foo/train.label --mode=hamming --bias=log
+python report.py --outdir=$foo --assembled=$foo/recons_hamming_log.txt --labels=$foo/test.label --mode=hamming --bias=log
 
-
-
-
+python ./assemble.py --outdir=$foo --f_inference="$foo/output_test" --fragnum=5 --mode="both" --bias=logsin
+echo "edit logsin"
+python report.py --outdir=$foo --assembled=$foo/recons_edit_logsin.txt --labels=$foo/test.label --mode=edit --bias=logsin
+echo "hamming logsin"
+python report.py --outdir=$foo --assembled=$foo/recons_hamming_logsin.txt --labels=$foo/test.label --mode=hamming --bias=logsin
 
 done
+#/Users/alex/Desktop
