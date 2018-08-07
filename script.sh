da267ce2c709a9480fee16f739282b3925276198
Alex <alexandre.leon.contact@gmail.com>
2018-08-07 11:26:08 -0700

    h

--- script.sh
+++ script.sh
@@ -2,28 +2,30 @@
 
 #make it a while loop
 
-foo='/Users/alex/Desktop/seq2t100by20_01_biadam'
+foo='/Users/alex/Desktop/seq100by20_02_biadam_bahdanau'
+bar='data100by20_02'
+tar='inference'
 
 echo $foo; 
 
-python ./assemble.py --outdir=$foo --f_inference="$foo/output_test" --fragnum=5 --mode="both" --bias=sin
+python ./assemble.py --outdir=$foo/$tar --f_inference="$foo/output_test" --fragnum=5 --mode="both" --bias=sin
 
 echo "edit sin"
-python report.py --outdir=$foo --assembled=$foo/recons_edit_sin.txt --labels=$foo/test.label --mode=edit --bias=sin
+python report.py --outdir=$foo/$tar --assembled=$foo/$tar/recons_edit_sin.txt --labels=$foo/$bar/test.label --mode=edit --bias=sin
 echo "hamming sin"
-python report.py --outdir=$foo --assembled=$foo/recons_hamming_sin.txt --labels=$foo/test.label --mode=hamming --bias=sin
+python report.py --outdir=$foo/$tar --assembled=$foo/$tar/recons_hamming_sin.txt --labels=$foo/$bar/test.label --mode=hamming --bias=sin
 
-python ./assemble.py --outdir=$foo --f_inference="$foo/output_test" --fragnum=5 --mode="both" --bias=log
+python ./assemble.py --outdir=$foo/$tar --f_inference="$foo/output_test" --fragnum=5 --mode="both" --bias=log
 echo "edit log"
-python report.py --outdir=$foo --assembled=$foo/recons_edit_log.txt --labels=$foo/test.label --mode=edit --bias=log
+python report.py --outdir=$foo/$tar --assembled=$foo/$tar/recons_edit_log.txt --labels=$foo/$bar/test.label --mode=edit --bias=log
 echo "hamming log"
-python report.py --outdir=$foo --assembled=$foo/recons_hamming_log.txt --labels=$foo/test.label --mode=hamming --bias=log
+python report.py --outdir=$foo/$tar --assembled=$foo/$tar/recons_hamming_log.txt --labels=$foo/$bar/test.label --mode=hamming --bias=log
 
-python ./assemble.py --outdir=$foo --f_inference="$foo/output_test" --fragnum=5 --mode="both" --bias=logsin
+python ./assemble.py --outdir=$foo/$tar --f_inference="$foo/output_test" --fragnum=5 --mode="both" --bias=logsin
 echo "edit logsin"
-python report.py --outdir=$foo --assembled=$foo/recons_edit_logsin.txt --labels=$foo/test.label --mode=edit --bias=logsin
+python report.py --outdir=$foo/$tar --assembled=$foo/$tar/recons_edit_logsin.txt --labels=$foo/$bar/test.label --mode=edit --bias=logsin
 echo "hamming logsin"
-python report.py --outdir=$foo --assembled=$foo/recons_hamming_logsin.txt --labels=$foo/test.label --mode=hamming --bias=logsin
+python report.py --outdir=$foo/$tar --assembled=$foo/$tar/recons_hamming_logsin.txt --labels=$foo/$bar/test.label --mode=hamming --bias=logsin
 
-done
+#done
 #/Users/alex/Desktop
