ae8d072d64b2c1be6cf0d336ec6c4c46e096097a
Alex <alexandre.leon.contact@gmail.com>
2018-08-14 17:31:32 -0700

    added new scripts

--- script9.sh
+++ script9.sh
@@ -4,10 +4,10 @@
 
 
 foo='/Users/alex/Documents/GitHub/exp_saves/infer_using_seq100by20_04' #parent directory
-bar='data100by20_04' #place data folder in the foo
-tar='inference_data100by20_04_assemble9' #name of output dir
-car='test100_04_infer' #name of inference file
-fragnum=5
+bar='data1000by20_04' #place data folder in the foo
+tar='inference_data1000by20_04_assemble9' #name of output dir
+car='test1000_04_infer' #name of inference file
+fragnum=50
 over=0.4
 
 
